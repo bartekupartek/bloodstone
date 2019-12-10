522c8e10d61cc63408343523e5f71fe63834d1f6
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2019-12-11 01:01:29 +0800

    update keybase

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,7 @@ config :bloodstone, BloodstoneWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "salty-falls-27943", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
