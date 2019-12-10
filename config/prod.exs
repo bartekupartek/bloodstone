b6513472948a2776000ef6c6b45c468e4493c3c4
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2019-12-11 01:23:42 +0800

    change

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :bloodstone, BloodstoneWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "salty-falls-27943", port: 443],
+  url: [scheme: "https", host: "salty-falls-27943.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
